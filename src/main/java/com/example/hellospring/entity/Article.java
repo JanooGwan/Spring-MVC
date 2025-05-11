f0d25e8a62b500750e8724f5262e464af9021816
JanooGwan <janoogwan39@gmail.com>
2025-05-11 14:00:19 +0900

    feat : Article Entity 작성

--- src/main/java/com/example/hellospring/entity/Article.java
+++ src/main/java/com/example/hellospring/entity/Article.java
@@ -1,5 +1,81 @@
 package com.example.hellospring.entity;
 
-public class Article {
+import java.time.LocalDateTime;
 
+public class Article {
+    Long id;
+    Long writerId;
+    Long boardId;
+    String title;
+    String content;
+    LocalDateTime createdDate;
+    LocalDateTime modifiedDate;
+
+    public Article(
+            Long id, Long writerId, Long boardId, String title, String content, LocalDateTime createdDate, LocalDateTime modifiedDate
+    ) {
+        this.id = id;
+        this.writerId = writerId;
+        this.boardId = boardId;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getWriterId() {
+        return writerId;
+    }
+
+    public void setWriterId(Long writerId) {
+        this.writerId = writerId;
+    }
+
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    public void setBoardId(Long boardId) {
+        this.boardId = boardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
