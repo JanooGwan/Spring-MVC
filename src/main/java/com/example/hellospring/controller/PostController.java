d3484afeb75e93392d799da9f3048a79f772af50
JanooGwan <janoogwan39@gmail.com>
2025-05-12 00:53:30 +0900

    update : 전체적인 오류 수정

--- src/main/java/com/example/hellospring/controller/PostController.java
+++ src/main/java/com/example/hellospring/controller/PostController.java
@@ -1,9 +1,11 @@
 package com.example.hellospring.controller;
 
 import com.example.hellospring.service.ArticleService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class PostController {
     private final ArticleService articleService;
 
